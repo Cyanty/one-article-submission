49c0b3c9cef89da2a0afb3debfd82e016f9313ef
Cyanty <liucaiyu96@gmail.com>
2024-12-27 16:30:18 +0800

    v1.0.0

--- config/base_config.py
+++ config/base_config.py
@@ -1,50 +1,18 @@
-# # 博客园metablog配置
-# CNBLOGS_METABLOG_CONFIG = {
-#     "url": "https://rpc.cnblogs.com/metaweblog/xxx",
-#     "appkey": "xxx",
-#     "usr": "xxx",
-#     "passwd": "xxx",
-#     "blogid": "xxx"
-# }
-#
-# # 微信公众号
-# wechat_public_account = {
-#     "grant_type": "client_credential",
-#     "appid": "xxxxx",
-#     "secret": "xxxxx"
-# }
-#
-# # halo publish 个人令牌
-# halo_publish_token = "xxxxx"
-
-
 # 博客园metablog配置
 CNBLOGS_METABLOG_CONFIG = {
-    "url": "https://rpc.cnblogs.com/metaweblog/cyanty",
-    "appkey": "Cyan-Rus",
-    "usr": "Cyan-rus",
-    "passwd": "F2929459DA975069E219D443FDD35A5C09CD0D6B2CD60D670C937EA781C499F8",
-    "blogid": "834696"
+    "url": "https://rpc.cnblogs.com/metaweblog/xxx",
+    "appkey": "xxx",
+    "usr": "xxx",
+    "passwd": "xxx",
+    "blogid": "xxx"
 }
 
 # 微信公众号
 wechat_public_account = {
     "grant_type": "client_credential",
-    "appid": "wxb5f9b5af73824543",
-    "secret": "32571062de44484f991f167753e9b523"
+    "appid": "xxxxx",
+    "secret": "xxxxx"
 }
 
 # halo publish 个人令牌
-halo_publish_token = "pat_eyJraWQiOiI0UDhBdHdQN252VlZDaGdZcmFtOFoxeWF6blhhaGxOcEF1QzhVeHlYNGZFIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwOTAvIiwic3ViIjoibGN5cnVzIiwiaWF0IjoxNzMxNTY2MzgyLCJqdGkiOiI3YTgwYjU5ZS02NGM1LTM4M2YtNzA5OC1hYzM5YzBiZDliMjEiLCJwYXRfbmFtZSI6InBhdC1sY3lydXMtZEtVdFcifQ.kzadZxv72JPDg8Q9nMEWVDw6vHmwwPBsGQmU_xkUbDlG8u5hWimDZ7fTSpab25GZj-bT9oTTrlDIRtToF8Egj9DrVmN4wZdPMmpzXrqP2svHQAwJlG-zES5DMWbeVg1ftagbU80VFHpQt9L1wec9pKeJ-exaTxWaPFTj73BfvOs2VepqTZU9bKT4zfpHpd7_YgXsSHmnq2yOJwHvCHdGeGnFNP1pb94TLFkgoMxUOWCg1O2rLSErPzXIFfnRms1dStv8yH-Xwh8IjW1aIaCi36ceclY_oZyXhLDAVNorPwgTnafAuLo1ZH0H4c5JgvPFXCW5HhePXplMNeWFWKi369Kr2BUxxmI_u_Wq_U-rpavAUH_kbbYw4_9EtJyJKtUEZKhxuY45n-6DVsNg7MPwj4Mj4O-NCkeU-eeeJYItfK_B7r_cxprlcw-hhLlAisNTu_P51xP5syyy6Aqbqkv9Et6ReGUzuRVEr1onuTHWbkyUAbM6wJ6K_W1aVEGNaOGOiTcl44whg4AAFMABNNhk7UySZxSiK1C8pQK6qXkcu1mIpIBqfY19X78gR8JMHG1ILrBCc1yV9E61CFC2SSFR_hscXNypBROloPKWYrdBGII8FRQJl7_ENlyOO0Kr6yVJIJiyjAxXXh_pCiie4I2QVqMJmTHUmbK1lhz0B2_2hc4"
-
-
-# 星火认知大模型
-# 星火认知大模型Spark Max的URL值，其他版本大模型URL值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
-SPARKAI_URL = 'wss://spark-api.xf-yun.com/v1.1/chat'
-# 星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
-SPARKAI_APP_ID = 'a6bf0d5f'
-SPARKAI_API_SECRET = 'MzA4M2MyYWM2MTI1YmNmZTA0ZmM3MmIw'
-SPARKAI_API_KEY = '9cbdad31fda8586f1dc36ab1f0523c52'
-# 星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
-SPARKAI_DOMAIN = 'general'
-
+halo_publish_token = "xxxxx"
